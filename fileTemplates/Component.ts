92c3798d89863d2fc16af75e72d6a40b19bf3471
cchia <casperchiakyechuen@gmail.com>
2017-02-16 10:00:53 +1100

    WS-171.3019.10 <cchia@SW140512 Create _@user_Material Theme - Default.icls, IntelliLang.xml, ide.general.xml, git.xml, _@user_Material Theme - Darker.icls, project.default.xml, diff.xml, Default copy.xml, other.xml, Zen CSS.xml, Default.xml, AngularJS .xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, Darcula copy2.icls, filetypes.xml, keymap.xml, xsl.xml, code.style.schemes.xml, plugin_ui.xml, watcherDefaultTasks.xml, colors.scheme.xml, Dart.xml, Zen HTML.xml, Darcula copy.icls, github_settings.xml, Module.ts, abbreviations.xml, Default.xml, laf.xml, jdk.table.xml, Meteor HTML.xml, path.macros.xml, Zen XSL.xml, Default _1_.xml, Handlebars.xml, web-browsers.xml, vcs.xml, editor.codeinsight.xml, Component.ts, editor.xml, usageView.xml, debugger.xml

---
+++ fileTemplates/Component.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by ${USER} on ${DATE}.
+ */
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+   selector: '#[[$selector$]]#',
+   styleUrls: ['#[[$selector$]]#.style.css'],
+   templateUrl: '#[[$selector$]]#.template.html'
+})
+
+export class #[[$Class$]]# implements OnInit{
+
+   constructor(){}
+
+   ngOnInit(){
+
+   }
+}
---
+++ fileTemplates/Module.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by ${USER} on ${DATE}.
+ */
+import {NgModule} from "@angular/core";
+
+@NgModule({
+   declarations: [],
+   imports: [],
+   exports: [],
+   providers: []
+})
+
+export class #[[$Class$]]#{
+   constructor(){}
+}
